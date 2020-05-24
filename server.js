a798767e29c6be46b5c7fcc1791c42e8f30b74ac
Stephmarsjs <54284220+Stephmarsjs@users.noreply.github.com>
2020-05-24 00:41:41 -0400

    fixed posts route

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.get('/', (req, res) => res.send('API Running'));
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('api/profile', require('./routes/profile'));
-app.use('api/profile', require('./routes/api/posts'));
+app.use('api/posts', require('./routes/api/posts'));
 
 const PORT = process.env.PORT || 5000
 
