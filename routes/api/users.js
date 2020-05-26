0f3e63ef8a53f5bb128f7c273a5385d0030d1d5b
Stephmarsjs <54284220+Stephmarsjs@users.noreply.github.com>
2020-05-25 23:24:59 -0400

    fixed error in User file

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express')
 const router = express.Router();
 const { check, validationResult } = require('express-validator/check');
 
+const User = require('../../models/User')
+
 // route POST api/users
 // This will register user 
 // access value Public
@@ -9,14 +11,23 @@ router.post('/',
 [
     check('name', 'Name is required').not().isEmpty(),
     check('email', 'Please include a valid email').isEmail(),
-    check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6})
+    check('password', 'Please enter a password with 6 or more characters'
+    ).isLength({ min: 6})
 ],
-(req, res) => {
+async (req, res) => {
   const errors = validationResult(req);
   if(!errors.isEmpty()){
       return res.status(400).json({ errors: errors.array() });
   }
 
+  const { name, email, password } = req.body;
+
+  try {
+    let user = await User.findOne({ email });
+
+    if (user) {
+        res.status(400).json({ errors: [{ msg: 'User already exists' }] });
+    }
   // See if user exists
 
   // get users gravatar
@@ -24,7 +35,12 @@ router.post('/',
   //encrypt password
 
   // return jasonwebtoken
-  
+
+  } catch(err) {
+      console.error(err.messaage);
+      res.status(send('Server error'))
+  }
+
   res.send('User Route');
   }
 );
