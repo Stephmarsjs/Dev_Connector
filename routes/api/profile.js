54990846ed406bd50dbd999a6449248546dd68d9
Stephmarsjs <54284220+Stephmarsjs@users.noreply.github.com>
2020-05-28 13:25:02 -0400

    modifying files with sensitive info

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router();
-const auth = require('../../middleware/auth')
-const { check, validationResult } = require('express-validator/check')
+const auth = require('../../middleware/auth');
+const { check, validationResult } = require('express-validator/check');
 
 // Post request api/profile/me
 // GET update user profile
@@ -34,14 +34,22 @@ router.get('/me', auth, async (req,res) => {
 // private
 
 router.post ('/', [auth, [
-    check('status', 'Status is required').not().isEmpty(),
-    check('skills', 'Skills is required').not().isEmpty()
-]], async (req, res) => {
+    check('status', 'Status is required')
+    .not()
+    .isEmpty(),
+    check('skills', 'Skills is required')
+    .not()
+    .isEmpty()
+
+   ]
+], 
+async (req, res) => {
     const errors = validationResult(req);
     if(!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
     }
 
-})
+  }
+)
 
-module.exports = router;
+module.exports = router
