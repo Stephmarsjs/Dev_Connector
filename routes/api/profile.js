768c248f80d22bad414c050957956afd09b7c4a9
Stephmarsjs <54284220+Stephmarsjs@users.noreply.github.com>
2020-06-03 23:53:34 -0400

    updated error in /me route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -12,11 +12,10 @@ const User = require('../../models/User');
 
 router.get('/me', auth, async (req,res) => {
     try {
-      const profile = await (await Profile.findOne({ user: req.user.id }).populate(
+      const profile = await Profile.findOne({ user: req.user.id }).populate(
         'user', 
         ['name', 'avatar'])
         
-      )
 
       if(!profile) {
           return res.status(400).json({ msg: 'There is no profile for this user' });
